1505d2a11e2727195ce5b36d3574ba35f6e881ce
Jeffery2k5 <jbatz6175@floridapoly.edu>
2024-11-14 21:19:54 -0500

    Initial commit: Add Sine Wave Model Script To File

---
+++ sine_wave_model.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define the sine wave parameters
+frequency = 5  # Frequency in Hz
+amplitude = 1  # Amplitude of the wave
+time = np.linspace(0, 1, 500)  # Time vector
+
+# Generate the sine wave
+sine_wave = amplitude * np.sin(2 * np.pi * frequency * time)
+
+# Plot the sine wave
+plt.plot(time, sine_wave)
+plt.title("Sine Wave")
+plt.xlabel("Time (s)")
+plt.ylabel("Amplitude")
+plt.grid(True)
+plt.show()
